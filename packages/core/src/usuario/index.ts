15da889065d9520418be9cfd89de06be03c25f72
IgorCerq22 <igor.cerqueira@jussi.com.br>
2024-12-26 17:54:11 -0300

    separando regras do backend para o core, nomenclatura

--- packages/core/src/usuario/index.ts
+++ packages/core/src/usuario/index.ts
@@ -1,5 +1,6 @@
 import Usuario from './model/Usuario'
+import RepositorioUsuario from './provider/RepositorioUsuario'
 import RegistrarUsuario from './service/RegistrarUsuario'
 
+export { RegistrarUsuario, RepositorioUsuario }
 export type { Usuario }
-export { RegistrarUsuario }
--- packages/core/src/usuario/service/RegistrarUsuario.ts
+++ packages/core/src/usuario/service/RegistrarUsuario.ts
@@ -1,7 +1,16 @@
 import CasoDeUso from '../../shared/CasoDeUso'
+import Usuario from '../model/Usuario'
+import RepositorioUsuario from '../provider/RepositorioUsuario'
 
 export default class RegistrarUsuario implements CasoDeUso {
-  async executar(entrada: any): Promise<any> {
-    return 'ok'
+  constructor(private readonly repo: RepositorioUsuario) {}
+  async executar(usuario: Usuario): Promise<any> {
+    const usuarioExistente = await this.repo.buscarPorEmail(usuario.email)
+
+    if (usuarioExistente) {
+      throw new Error('Usuário já existe')
+    }
+
+    await this.repo.salvar({ ...usuario, barbeiro: false })
   }
 }
