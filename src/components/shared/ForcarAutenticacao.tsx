3f53646542e96a573dea697ea43d3a5a26b8c461
IgorCerq22 <igor.cerqueira@jussi.com.br>
2025-01-07 11:22:58 -0300

    ajustando caminho, usando suspense fallback (usesearchparams)

--- src/components/shared/ForcarAutenticacao.tsx
+++ src/components/shared/ForcarAutenticacao.tsx
@@ -3,17 +3,17 @@
 import { usePathname, useRouter } from 'next/navigation'
 
 import useSessao from '@/data/hooks/useSessao'
+import Processando from './Processando'
 
 export default function ForcarAutenticacao(props: any) {
   const { usuario, carregando } = useSessao()
   const router = useRouter()
   const caminho = usePathname()
 
-  if (carregando && !usuario?.email)
-    return <div className="">Carregando...</div>
+  if (carregando && !usuario?.email) return <Processando />
   if (!usuario?.email) {
     router.push(`/entrar?destino=${caminho}`)
-    return <div className="">Redirecionand...</div>
+    return <Processando />
   }
 
   return props.children
