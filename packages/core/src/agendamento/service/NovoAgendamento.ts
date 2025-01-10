e8f908489e0e8b868f1c64f5b2a96e70f1438150
IgorCerq22 <igor.cerqueira@jussi.com.br>
2025-01-10 18:04:51 -0300

    novo agendamento - integraçao backend

--- packages/core/src/agendamento/service/NovoAgendamento.ts
+++ packages/core/src/agendamento/service/NovoAgendamento.ts
@@ -4,22 +4,20 @@ import CasoDeUso from '../../shared/CasoDeUso'
 import Agendamento from '../model/Agendamento'
 import RepositorioAgendamento from '../provider/RepositorioAgendamento'
 
-type NovoAgendamentoParams = {
+type Entrada = {
   agendamento: Agendamento
   usuario: Usuario
 }
 
-export default class NovoAgendamento
-  implements CasoDeUso<NovoAgendamentoParams, void>
-{
+export default class NovoAgendamento implements CasoDeUso<Entrada, void> {
   constructor(private readonly repo: RepositorioAgendamento) {}
 
-  async executar(params: NovoAgendamentoParams): Promise<void> {
-    const { agendamento, usuario } = params
+  async executar(entrada: Entrada): Promise<void> {
+    const { agendamento, usuario } = entrada
 
-    if (usuario.email !== agendamento.usuario.email)
+    if (usuario.id !== agendamento.usuario.id)
       throw new Error('Não é possivel criar um agendamento para outro usuário')
 
-    await this.repo.criar(params.agendamento)
+    await this.repo.criar(agendamento)
   }
 }
