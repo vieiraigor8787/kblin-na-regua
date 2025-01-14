40d5636344240532026952e9799d9a3d738bfc58
IgorCerq22 <igor.cerqueira@jussi.com.br>
2025-01-14 19:28:51 -0300

    campo data e hora - core - funcao de slots

--- packages/core/src/utils/AgendaUtils.ts
+++ packages/core/src/utils/AgendaUtils.ts
@@ -8,6 +8,16 @@ export default class AgendaUtils {
     }
   }
 
+  static duracaoTotal(servicos: { qtdeSlots: number }[]) {
+    const duracao = servicos.reduce(
+      (total, servico) => total + servico.qtdeSlots * 15,
+      0
+    )
+    const horas = Math.floor(duracao / 60)
+    const minutos = duracao % 60
+    return `${horas}h ${minutos.toString().padStart(2, '0')}m`
+  }
+
   static gerarHorarios(horas: number[]): string[] {
     return horas.reduce((horarios, hora) => {
       const horariosDaHora = this.minutos.map((minuto) => {
@@ -21,4 +31,13 @@ export default class AgendaUtils {
   }
 }
 
-console.log(AgendaUtils.horariosDoDia())
+// console.log(
+//   AgendaUtils.duracaoTotal([
+//     {
+//       qtdeSlots: 2,
+//     },
+//     {
+//       qtdeSlots: 3,
+//     },
+//   ])
+// )
