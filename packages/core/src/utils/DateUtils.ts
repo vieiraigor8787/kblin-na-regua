ec8b6cdbbff347fb348a4d00662928f5c699fb8d
IgorCerq22 <igor.cerqueira@jussi.com.br>
2025-01-15 11:53:31 -0300

    core: utils - aplicar horario

--- packages/core/src/utils/DateUtils.ts
+++ packages/core/src/utils/DateUtils.ts
@@ -18,5 +18,16 @@ export default class DateUtils {
     }
     return dias
   }
+
+  //new date, '09:15'
+  static aplicarHorario(data: Date, horario: string) {
+    const novaData = new Date(data)
+    const [hora, min] = horario.split(':').map(Number)
+
+    novaData.setHours(hora, min, 0, 0)
+    return novaData
+  }
 }
+
 // console.log(DateUtils.proximosDias(7, false))
+// console.log(DateUtils.aplicarHorario(new Date(), '15:45').toLocaleTimeString())
