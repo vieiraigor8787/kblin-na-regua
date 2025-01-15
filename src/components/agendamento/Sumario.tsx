2b6c0143fa69b4ad9f7ff5742c4c20cba728640d
IgorCerq22 <igor.cerqueira@jussi.com.br>
2025-01-15 13:22:47 -0300

    sucesso - estilizando sumario

--- src/components/agendamento/Sumario.tsx
+++ src/components/agendamento/Sumario.tsx
@@ -1,7 +1,22 @@
+import { IconCalendar } from '@tabler/icons-react'
+
 export default function Sumario() {
   return (
-    <div className="flex flex-col bg-zinc-900 w-96">
-      <span>Sumario</span>
+    <div className="flex flex-col bg-zinc-900 w-96 rounded-lg">
+      <div className="flex items-center p-4 gap-2 border-b border-zinc-700">
+        <div className="flex justify-center items-center bg-zinc-700 h-9 w-9 rounded-full">
+          <IconCalendar size={20} stroke={1} />
+        </div>
+
+        <div className="flex flex-col">
+          <span className="font-bold text-zinc-300">
+            Detalhes do agendamento
+          </span>
+          <span className="text-xs text-zinc-500 leading-3">
+            Será um prazer atender você!
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
